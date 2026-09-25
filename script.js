5f058b9d938a889fbc37fab6c6a5c835af3bc886
Abdul Mulla <abdulmulla8071@gmail.com>
2026-09-25 17:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-	const radius = Number(document.getElementById("radius").value);
-	const volumn = Number(document.getElementById("volumn");
+const form = document.querySelector("form");
 
-	if(isNaN(radius) || radius < 0){
-		volumn.value = "NaN";
-	}
-	else{
-			const result = (4 / 3) * Math.PI * radius ** 3;
-		volumn.value = result.toFixed(4);
-	}
-} 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+const radius = document.getElementById("radius");
+const volume = document.getElementById("volume");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const r = Number(radius.value);
+
+    const result = (4 / 3) * Math.PI * r * r * r;
+
+    volume.value = result.toFixed(4);
+});
