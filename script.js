46bc7d02bc59a27b0eef56c928aa1f3ad45105dd
Abdul Mulla <abdulmulla8071@gmail.com>
2026-09-25 17:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const form = document.querySelector("form");
-
+const form = document.getElementById("MyForm");
 const radius = document.getElementById("radius");
 const volume = document.getElementById("volume");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const r = Number(radius.value);
+    const r = parseFloat(radius.value);
+
+    if (r < 0) {
+        volume.value = "NaN";
+        return;
+    }
 
     const result = (4 / 3) * Math.PI * r * r * r;
 
